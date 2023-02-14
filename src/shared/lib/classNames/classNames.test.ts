770ea013e0de15a9b464be7fbf147d4687017e90
Mark-front <76992085+Mark-front@users.noreply.github.com>
2023-02-14 20:49:09 +0300

    base unit test on classNames

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,18 @@
+import { classNames } from './classNames'
+
+describe('classNames', () => {
+    test('with only first params', () => {
+        expect(classNames('someClass', {}, [])).toBe('someClass')
+    })
+    test('with additional params', () => {
+        const expected = 'someClass lols'
+        expect(classNames('someClass', {}, ['lols'])).toBe(expected)
+    })
+    test('with mods', () => {
+        const expected = 'someClass lols is-hovered'
+        expect(classNames(
+            'someClass',
+            { 'is-hovered': true, 'visually-hidden': false },
+            ['lols'])).toBe(expected)
+    })
+})
