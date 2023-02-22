196203ed267cc8568df83ee882c133c0c605f9b0
Mark-front <76992085+Mark-front@users.noreply.github.com>
2023-02-22 22:44:07 +0300

    fix unit test sidebar

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,22 @@
+import {type ReactNode} from 'react';
+import {I18nextProvider} from 'react-i18next';
+import i18nForTest from '../../../config/i18n/i18nForTest';
+import {render} from '@testing-library/react';
+import {MemoryRouter} from "react-router-dom";
+
+export interface ComponentRenderOption {
+    route?: string
+}
+
+export function componentRender(component: ReactNode, options: ComponentRenderOption = {}) {
+    const {
+        route = '/',
+    } = options;
+    return render(
+        <MemoryRouter initialEntries={[route]}>
+            <I18nextProvider i18n={i18nForTest}>
+                {component}
+            </I18nextProvider>
+        </MemoryRouter>
+    )
+}
