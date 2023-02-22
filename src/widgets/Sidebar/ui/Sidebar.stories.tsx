e9d4a8f42713af7739f0111589a3b0811e404524
Mark-front <76992085+Mark-front@users.noreply.github.com>
2023-02-22 22:57:25 +0300

    fix unit test sidebar

--- src/widgets/Sidebar/ui/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar.stories.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { type ComponentStory, type ComponentMeta } from '@storybook/react';
+import {type ComponentStory, type ComponentMeta} from '@storybook/react';
 
-import { Sidebar } from '@/widgets/Sidebar';
-import { ThemeDecorator } from '@/shared/config/storybook/decorators/ThemeDecorator/ThemeDecorator';
-import { Theme } from '@/shared/ui/ThemeProvider';
+import {Sidebar} from '@/widgets/Sidebar';
+import {ThemeDecorator} from '@/shared/config/storybook/decorators/ThemeDecorator/ThemeDecorator';
+import {Theme} from '@/shared/ui/ThemeProvider';
+import {
+    MemoryRouterDecorator,
+} from "@/shared/config/storybook/decorators/MemoryRouterDecorator/MemoryRouterDecorator";
 
 export default {
     title: 'widgets/Sidebar',
     component: Sidebar,
     argTypes: {},
+    decorators: [MemoryRouterDecorator],
 } as ComponentMeta<typeof Sidebar>;
 
 const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />;
