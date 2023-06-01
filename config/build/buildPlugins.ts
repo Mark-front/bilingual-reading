743851e0825b4e89e6e77f4218e10efe930736cb
Mark-front <76992085+Mark-front@users.noreply.github.com>
2023-06-01 16:00:45 +0300

    add CircularDependencyPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,6 +5,7 @@ import { DefinePlugin, HotModuleReplacementPlugin, ProgressPlugin, type WebpackP
 import { type BuildOptions } from './types/config'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 import CopyPlugin from 'copy-webpack-plugin';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 
 export function buildPlugins(options: BuildOptions): WebpackPluginInstance[] {
     const { paths, isDev, isAnalyze, apiUrl, project } = options
@@ -27,6 +28,19 @@ export function buildPlugins(options: BuildOptions): WebpackPluginInstance[] {
             patterns: [
                 { from: paths.locales, to: paths.buildLocales },
             ],
+        }),
+        new CircularDependencyPlugin({
+            // exclude detection of files based on a RegExp
+            exclude: /node_modules/,
+            // include specific files based on a RegExp
+            include: /dir/,
+            // add errors to webpack instead of warnings
+            failOnError: true,
+            // allow import cycles that include an asyncronous import,
+            // e.g. via import(/* webpackMode: "weak" */ './file.js')
+            allowAsyncCycles: false,
+            // set the current working directory for displaying module paths
+            cwd: process.cwd(),
         }),
     ]
     if (isDev) {
